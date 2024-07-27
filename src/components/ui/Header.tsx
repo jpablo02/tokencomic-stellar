c39579ab6f66bd9f6653c0b2ae1e94c741ab6ded
juan <jpredcoin@gmail.com>
2024-07-27 18:58:30 -0500

    avanzando en nav var

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -4,22 +4,26 @@ import { Button } from "./button";
 
 import Nav from "./Nav";
 
-
-
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white bg-pink-50/20">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         {/*logo*/}
         <Link href="/src/public/drum.png">
           <h1 className="text 4-xl font-semibold">
             Pablodrum <span className="text-accent-DEFALUT">.</span>
           </h1>
         </Link>
+
         {/*dektop nav*/}
-        <Nav />
-
-
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <button>Hire me</button>
+          </Link>
+        </div>
+        {/*mobile nav*/}
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
