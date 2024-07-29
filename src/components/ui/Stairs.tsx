5b75217a14d99d1c291ebc653bab905912511b23
juan <jpredcoin@gmail.com>
2024-07-29 17:41:45 -0500

    adelantando stairs

---
+++ src/components/ui/Stairs.tsx
@@ -0,0 +1,24 @@
+import{animate, motion}from "framer-motion"
+
+const stairAnimation ={
+    initial:{
+        top:"0%"
+    },
+
+    animate:{
+        top:"100%"
+    },
+
+    exit:{
+        top:["100%","0%"],
+    },
+
+};
+
+const Stairs = () => {
+  return (
+    <div>Stairs</div>
+  )
+}
+
+export default Stairs
