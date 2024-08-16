ffe37be6808c74abb9bf42df9f4ae11b9f888d24
juan <jpredcoin@gmail.com>
2024-08-16 12:54:01 -0500

    definiendo transiciones

--- src/app/coomingSoon/page.jsx
+++ src/app/coomingSoon/page.jsx
@@ -3,22 +3,25 @@ import React from "react";
 
 const about = () => {
   return (
-    <section className="col-span-1">
-      <div className="text-center xl:text-center order-2 xl:order-none pr-0">
-        <h1 className="h1 mb-4">
-          <div className="bg-orange-400 inline-block rounded-2xl">
-            <span className="text-black">Coming</span>
+    <section className="h-full bg-[#1E293B] justify-center ">
+      <div className="container mx-auto h-full ">
+        <div className="bg-[#1E293B] flex flex-col xl:flex-row items-center justify-center xl:pt-6 xl:pb-6 ">
+          {/*text */}
+          <div className="text-center xl:text-center order-1 xl:order-none pr-0 pt-4 text-2xl">
+            <h1 className="h1 mb-4 items-center">
+              <div className="bg-[#f58240] inline-block rounded-2xl items-center">
+                <span className="text-black items-center">Coming</span>
+              </div>
+              <br />
+              {"Soon"}
+            </h1>
+            <h2 className=" text-2xl max-w-[500px] mb-9 text-white/80">
+              Working to Grow up!!!
+            </h2>
+
+            {/*boton */}
           </div>
-
-          <br />
-          {"Soon"}
-        </h1>
-      </div>
-
-      <div className="text-center xl:text-center order-2 xl:order-none pr-0">
-        <h2 className=" text-center text-2xl max-w-[500px] mb-9 text-white/80">
-          We are working to grow our community!!!
-        </h2>
+        </div>
       </div>
     </section>
   );
