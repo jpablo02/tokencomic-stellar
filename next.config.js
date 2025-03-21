e9379e17e385a912a37710081a8b437a058665f7
kali <kali@localhost>
2025-03-21 16:40:06 +0000

    haciendo prueba para integrar kit stellar wallets

--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'gateway.lighthouse.storage',
-      'bafybeihv6rzseuad62u67gtsihwwlx6mhia22cxudc3i7xia4gka4mzaxi.ipfs.dweb.link',
-      'bafybeiexjn4b7ewg2a7embuyrfooxqk7nsbkds3chcbqdgxkz3gbbybmuq.ipfs.dweb.link',
-      'bafybeifirjfkkekeqm3karvk2b33ysmtmealju7a2v24zhia4edhvpl2rm.ipfs.dweb.link',
-      'bafybeihaeqxsomey6atu77nvtqg7d7p6ycekstgweiostkwpknz6idujdi.ipfs.dweb.link', // Agregado
-      'bafybeihie6nphrd275hp4pxi5pubwrmbkanpgocnhnfmu7zp6bcemragae.ipfs.dweb.link', // Agregado
-      'chocolate-legislative-lamprey-152.mypinata.cloud',
-      'bafybeifgvjz7fx7q7ubbbos32ffliiu7c7lsykcsdkvcmitjfkt3gksvge.ipfs.dweb.link',
-      'bafybeifypdd6ffmryu6mbcofzkj5su7gobqorya3deq7umxlmxouihnbwa.ipfs.dweb.link', // Agregado este dominio
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gateway.lighthouse.storage"
+      },
+      {
+        protocol: "https",
+        hostname: "**.ipfs.dweb.link" // Usamos comodín para todos los subdominios IPFS
+      },
+      {
+        protocol: "https",
+        hostname: "chocolate-legislative-lamprey-152.mypinata.cloud"
+      }
     ],
   },
   reactStrictMode: true,
+  // Agrega esto para el error de módulos ESM
+  experimental: {
+    esmExternals: 'loose' // o 'true' si 'loose' no funciona
+  }
 }
 
 module.exports = nextConfig
